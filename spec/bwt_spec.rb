9b153828f2569d6e8910f989e85ca77247ce40e8
Jim Barnett <james.barnett.jr@gmail.com>
2015-04-16 19:50:59 -0500

    Adds UTF-8 test case

--- spec/bwt_spec.rb
+++ spec/bwt_spec.rb
@@ -21,4 +21,8 @@ describe Bwt do
   it "encodes and decodes" do
     decode(encode("banana")).must_equal "banana"
   end
+
+  it "encodes and decodes UTF-8" do
+    decode(encode("åßç∂")).must_equal "åßç∂"
+  end
 end
